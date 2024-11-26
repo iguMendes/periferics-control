402257d3d9ab07d4074300b73a386e8e1f14b4d8
igor pc <igorast113@gmail.com>
2024-11-26 20:03:41 -0300

    responsividade

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'hd': '1366px',  // HD
+        'fhd': '1920px', // FHD
+      },
+      fontSize: {
+        'hd': '14px',  // Tamanho de fonte para HD
+        'fhd': '18px', // Tamanho de fonte para FHD
+      },
+    },
   },
   plugins: [],
 };
