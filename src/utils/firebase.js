e1faa09a7e20f935bfd7fee21a9c1c3932837d11
igor pc <igorast113@gmail.com>
2024-11-26 19:13:36 -0300

    .env dos arquivos

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,30 +1,34 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { 
-  getFirestore, 
-  collection, 
-  addDoc, 
-  getDocs, 
-  setDoc, 
-  deleteDoc, 
-  doc, 
-  query, 
-  where, 
-            } from "firebase/firestore";
+import dotenv from 'dotenv';
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  setDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCblBU4w6qNzHwl07-6MRlfhIGWGgXaFRA",
-    authDomain: "controllerperifericos-3e9b2.firebaseapp.com",
-    projectId: "controllerperifericos-3e9b2",
-    storageBucket: "controllerperifericos-3e9b2.firebasestorage.app",
-    messagingSenderId: "801690815808",
-    appId: "1:801690815808:web:60a250147d7611d0ae7ea5",
-    measurementId: "G-M0RLLL03W9"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+dotenv.config();
 
 export const adicionarFuncionarioFirestore = async (nomeFuncionario) => {
   try {
@@ -68,43 +72,43 @@ export const adicionarPerifericoFirestore = async (tipoPeriferico, periferico) =
     const docRef = doc(perifericosRef, periferico.numSerie);
     await setDoc(docRef, periferico)
     console.log(`Periférico adicionado com numSerie: ${periferico.numSerie}`);
-    return periferico; 
+    return periferico;
   } catch (error) {
     console.error("Erro ao adicionar periférico:", error);
     throw error;
   }
 };
 
-  export const obterPerifericosFirestore = async (tipoPeriferico) => {
-    try {
-      const perifericosRef = collection(db, "perifericos", tipoPeriferico, "perifericos");
-      const querySnapshot = await getDocs(perifericosRef);
-  
-      return querySnapshot.docs.map((doc) => ({
-        id: doc.id, 
-        ...doc.data(),
-      }));
-    } catch (error) {
-      console.error("Erro ao obter periféricos:", error);
-      throw error;
-    }
-  };
+export const obterPerifericosFirestore = async (tipoPeriferico) => {
+  try {
+    const perifericosRef = collection(db, "perifericos", tipoPeriferico, "perifericos");
+    const querySnapshot = await getDocs(perifericosRef);
 
-  export const removerPerifericoFirestore = async (tipoPeriferico, numSerie) => {
-    try {
-      const perifericosRef = collection(db, "perifericos", tipoPeriferico, "perifericos");
-      const q = query(perifericosRef, where("numSerie", "==", numSerie));
-      const querySnapshot = await getDocs(q);
-      const deletePromises = querySnapshot.docs.map((doc) =>
-        deleteDoc(doc.ref)
-      );
-  
-      await Promise.all(deletePromises);
-      console.log(`Periférico com número de série ${numSerie} deletado com sucesso.`);
-    } catch (error) {
-      console.error("Erro ao deletar periférico:", error);
-      throw error;
-    }
-  };
+    return querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+  } catch (error) {
+    console.error("Erro ao obter periféricos:", error);
+    throw error;
+  }
+};
+
+export const removerPerifericoFirestore = async (tipoPeriferico, numSerie) => {
+  try {
+    const perifericosRef = collection(db, "perifericos", tipoPeriferico, "perifericos");
+    const q = query(perifericosRef, where("numSerie", "==", numSerie));
+    const querySnapshot = await getDocs(q);
+    const deletePromises = querySnapshot.docs.map((doc) =>
+      deleteDoc(doc.ref)
+    );
+
+    await Promise.all(deletePromises);
+    console.log(`Periférico com número de série ${numSerie} deletado com sucesso.`);
+  } catch (error) {
+    console.error("Erro ao deletar periférico:", error);
+    throw error;
+  }
+};
 
 export { app, analytics, db };
